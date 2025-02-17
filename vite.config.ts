37b69892a84881b2c7a7291060c0d01bff1dd21a
Orestis Zinelis <zinelisorestis@gmail.com>
2025-02-17 10:07:34 +0200

    Add packages & clean up

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+  },
   resolve: {
     alias: {
       '@src': '/src',
       '@components': '/src/components',
       '@utils': '/src/utils',
       '@hooks': '/src/hooks',
-      '@api': '/src/api',
+      '@services': '/src/services',
       '@layouts': '/src/layouts',
       '@pages': '/src/pages',
       '@contexts': '/src/contexts',
+      '@constants': '/src/constants',
     },
   },
 })
