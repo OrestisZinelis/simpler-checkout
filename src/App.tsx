b85addf9e3e72c8ec98852467fc2af7cc4e751d0
Orestis Zinelis <zinelisorestis@gmail.com>
2025-02-17 10:17:52 +0200

    Add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useReducer, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AppProvider } from '@contexts/AppProvider'
+import Header from '@components/Header'
+import StorePage from '@pages/StorePage'
+import OrderPage from '@pages/OrderPage'
+import { initialState, appReducer } from '@contexts/AppReducer'
+import { ToastProvider } from '@contexts/ToastProvider'
+import { CART_ITEMS_STORAGE_KEY } from '@constants/localStorage.const'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [state, dispatch] = useReducer(appReducer, initialState)
+
+  useEffect(() => {
+    const savedCart = localStorage.getItem(CART_ITEMS_STORAGE_KEY)
+    if (savedCart) {
+      const parsedCart = JSON.parse(savedCart)
+      dispatch({ type: 'SET_CART_ITEMS', payload: parsedCart })
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ToastProvider>
+      <AppProvider state={state} dispatch={dispatch}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<StorePage />} />
+            <Route path="/order/:orderId" element={<OrderPage />} />
+          </Routes>
+        </Router>
+      </AppProvider>
+    </ToastProvider>
   )
 }
 
